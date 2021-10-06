b06a2ab3d851dcec8c64172c6ba8fb4c2e3466f1
ABHINETWORK1 <88935139+ABHINETWORK1@users.noreply.github.com>
2021-10-06 22:30:54 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -153,7 +153,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="💫𝚄𝙿𝙻𝙾𝙰𝙳𝙴𝚁✨",
-                        url="https://t.me/ABHI_NETWORK"),
+                        url="https://t.me/SNEHABHI_SERVER"),
                     InlineKeyboardButton(
                         text="💫𝚃𝙰𝙻𝙺 𝚃𝙾 𝙾𝚆𝙽𝙴𝚁✨",
                         url="http://t.me/SNEHU_IS_MINE")
@@ -194,7 +194,7 @@ async def play(_, message: Message):
                     [
                         InlineKeyboardButton(
                         text="💫𝚄𝙿𝙻𝙾𝙰𝙳𝙴𝚁✨",
-                        url="https://t.me/ABHI_NETWORK"),
+                        url="https://t.me/SNEHABHI_SERVER"),
                     InlineKeyboardButton(
                         text="💫𝚃𝙰𝙻𝙺 𝚃𝙾 𝙾𝚆𝙽𝙴𝚁✨",
                         url="http://t.me/SNEHU_IS_MINE")
@@ -212,7 +212,7 @@ async def play(_, message: Message):
                         [
                             InlineKeyboardButton(
                         text="💫𝚄𝙿𝙻𝙾𝙰𝙳𝙴𝚁✨",
-                        url="https://t.me/ABHI_NETWORK"),
+                        url="https://t.me/SNEHABHI_SERVER"),
                     InlineKeyboardButton(
                         text="💫𝚃𝙰𝙻𝙺 𝚃𝙾 𝙾𝚆𝙽𝙴𝚁✨",
                         url="http://t.me/SNEHU_IS_MINE")
@@ -264,7 +264,7 @@ async def play(_, message: Message):
                 [
                     [
                         text="💫𝚄𝙿𝙻𝙾𝙰𝙳𝙴𝚁✨",
-                        url="https://t.me/ABHI_NETWORK"),
+                        url="https://t.me/SNEHABHI_SERVER"),
                     InlineKeyboardButton(
                         text="💫𝚃𝙰𝙻𝙺 𝚃𝙾 𝙾𝚆𝙽𝙴𝚁✨",
                         url="http://t.me/SNEHU_IS_MINE")
